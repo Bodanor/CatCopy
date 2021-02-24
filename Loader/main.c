6010d77c5c6f4616b70fcfec86bf06056fa3e3b4
Bodanor <papadopoulos.chris@icloud.com>
2021-02-24 13:36:11 +0100

    Initial Commit

---
+++ Loader/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Loader
+//
+//  Created by Christos Papadopoulos on 24/02/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
